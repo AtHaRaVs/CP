e4372c5820470c10bea58538ee04323e7483ee3e
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-04-30 23:42:10 +0530

    bottoms up

---
+++ dp/house_robber.cpp
@@ -0,0 +1,71 @@
+// top-down
+class Solution
+{
+public:
+    int solve(vector<int> &nums, int i, int n)
+    {
+        if (i >= n)
+            return 0;
+
+        int steal = nums[i] + solve(nums, i + 2, n);
+        int skip = solve(nums, i + 1, n);
+
+        return max(steal, skip);
+    }
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        return solve(nums, 0, n);
+    }
+};
+
+// memoize
+
+class Solution
+{
+public:
+    int solve(vector<int> &nums, int i, int n, vector<int> &dp)
+    {
+        if (i >= n)
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int steal = nums[i] + solve(nums, i + 2, n, dp);
+        int skip = solve(nums, i + 1, n, dp);
+
+        return dp[i] = max(steal, skip);
+    }
+    int rob(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        vector<int> dp(n + 1, -1);
+        return solve(nums, 0, n, dp);
+    }
+};
+
+// bottoms up defining a state
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> dp(n + 1, 0);
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 2; i <= n; i++)
+        {
+            int steal = nums[i - 1] + dp[i - 2];
+            int skip = dp[i - 1];
+
+            dp[i] = max(steal, skip);
+        }
+
+        return dp[n];
+    }
+};
