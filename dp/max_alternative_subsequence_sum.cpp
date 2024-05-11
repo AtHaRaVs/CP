d1fa7f16b7199c439b373d345abd9aa752652b4d
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-05-12 00:59:58 +0530

    sol

--- dp/max_alternative_subsequence_sum.cpp
+++ dp/max_alternative_subsequence_sum.cpp
@@ -30,32 +30,31 @@ public:
 class Solution
 {
 public:
-    long long solve(int idx, vector<int> &nums, bool flag, int n, vector<vector<long long>> &dp)
+    long long solve(int idx, bool flag, vector<int> &nums, int n, vector<vector<long long>> &dp)
     {
 
         if (idx >= n)
             return 0;
 
-        if (dp[idx][flag] != -1)
-            return dp[idx][flag];
+        if (dp[flag][idx] != -1)
+            return dp[flag][idx];
 
-        long long skip = solve(idx + 1, nums, flag, n, dp);
+        long long skip = solve(idx + 1, flag, nums, n, dp);
 
-        long long val = nums[idx];
+        int val = nums[idx];
         if (!flag)
             val = -val;
 
-        long long take = val + solve(idx + 1, nums, !flag, n, dp);
+        long long steal = solve(idx + 1, !flag, nums, n, dp) + val;
 
-        return dp[idx][flag] = max(skip, take);
+        return dp[flag][idx] = max(steal, skip);
     }
-
     long long maxAlternatingSum(vector<int> &nums)
     {
         int n = nums.size();
 
-        vector<vector<long long>> dp(n + 1, vector<long long>(2, -1));
+        vector<vector<long long>> dp(2, vector<long long>(n + 1, -1));
 
-        return solve(0, nums, true, n, dp);
+        return solve(0, true, nums, n, dp);
     }
 };
